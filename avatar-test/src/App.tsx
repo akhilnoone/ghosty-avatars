c1d67b347465423388c8fca829f39929a6f909f0
nooneakhil18 <nooneakhil@gmail.com>
2026-03-08 21:19:51 -0700

    increase nav avatar size to 32

--- avatar-test/src/App.tsx
+++ avatar-test/src/App.tsx
@@ -62,7 +62,7 @@ function App() {
     <>
       <nav className="nav">
         <div className="nav-logo">
-          <GhostyAvatar name="ghosty" size={28} borderRadius="6px" label="" />
+          <GhostyAvatar name="ghosty" size={32} borderRadius="6px" label="" />
           Ghosty<span> Avatar</span>
         </div>
         <div className="nav-links">
